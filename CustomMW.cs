d024136e2d2341a326c5896a574dfea4698a1e43
martawiszowata2000 <65867090+martawiszowata2000@users.noreply.github.com>
2021-04-26 19:32:13 +0200

    Dodaj pliki projektów.

---
+++ CustomMW.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMW
+{
+    public class CustomMW
+    {
+        private readonly RequestDelegate next;
+        public CustomMW(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            string ua = httpContext.Request.Headers["User-Agent"];
+            if (ua.Contains("Edg") || ua.Contains("EdgChromium") || ua.Contains("Trident"))
+            {
+                httpContext.Response.WriteAsync("Przegladarka nie jest obslugiwana");
+            }
+            return next(httpContext);
+        }
+    }
+}
